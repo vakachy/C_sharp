348f800f83f2c6fc819d48738b275feedb0ae4f7
Valery <vakachy@gmail.com>
2022-10-01 22:27:50 +0300

    Lecture_1 Example008_RunningDog added

---
+++ Lectures/Lecture_1/Example008_RunningDog/Program.cs
@@ -0,0 +1,28 @@
+﻿
+double distance = 2500;
+int firstFriendSpeed = 10;
+int secondFriendSpeed = 5;
+int dogsSpeed = 20;
+int count = 0;
+int friend = 1;
+double time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogsSpeed);
+        friend = 2;
+    }
+
+    if (friend == 2)
+    {
+        time = distance / (secondFriendSpeed + dogsSpeed);
+        friend = 2;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine(count);

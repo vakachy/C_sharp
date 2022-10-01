831745ffb0bd0b4890753ba7f648e777929aa47e
Valery <vakachy@gmail.com>
2022-10-01 23:01:57 +0300

    Lesson_1 Task_6 added

---
+++ Lessons/Lesson1/Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// 6. Выяснить является ли число чётным.
+
+int a = -30;
+
+if (a % 2 == 0)
+{
+    Console.Write("The number's even");
+}
+else
+{
+    Console.Write("The number's odd");
+}
---
+++ Lessons/Lesson1/Task_6/readme.md
@@ -0,0 +1,5 @@
+6. Выяснить является ли число чётным.
+
+[Task_6 -- code](Program.cs)  
+
+[Task_6 -- flowchart](flowchart.drawio.png)

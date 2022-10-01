9cef617ef76de1fac852b7c43ea42fd651b707f7
Valery <vakachy@gmail.com>
2022-10-01 22:50:41 +0300

    Lesson_1 Task_2 added

---
+++ Lessons/Lesson1/Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//2. Даны два числа. Показать большее и меньшее число
+
+int a = 20, b = 110;
+
+if (a > b)
+{
+    Console.WriteLine("a = " + a + " > " + "b = " + b);
+}
+else
+{
+    Console.WriteLine("a = " + a + " < " + "b = " + b);
+}
---
+++ Lessons/Lesson1/Task_2/readme.md
@@ -0,0 +1,5 @@
+2. Даны два числа. Показать большее и меньшее число
+
+[Task_2 -- code](Program.cs)  
+
+[Task_2 -- flowchart](flowchart.drawio.png)

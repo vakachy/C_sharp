2a7d92f269c9cbb7e8eea9e287b7b114efb58881
Valery <vakachy@gmail.com>
2022-10-02 10:14:18 +0300

    Homework: Lesson_1 Task_8 added

---
+++ Homeworks/Lesson_1/Task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 8:
+// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+int N = new Random().Next(2, 41);
+int currentNumber = 1;
+int lastNumber = N;
+
+Console.Write(" All even numbers from the given interval [1, " + N + "]: ");
+
+while (currentNumber <= lastNumber)
+{
+    if (currentNumber % 2 == 0)
+    {
+        Console.Write(currentNumber + " ");
+    }
+
+    currentNumber++;
+}

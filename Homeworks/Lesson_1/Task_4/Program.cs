70d5c35f6212c4c988e816ba9e0284a76989f4e4
Valery <vakachy@gmail.com>
2022-10-02 10:05:30 +0300

    Homework: Lesson_1 Task_4 added

---
+++ Homeworks/Lesson_1/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+int a = new Random().Next(0, 101);
+int b = new Random().Next(0, 101);
+int c = new Random().Next(0, 101);
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine("Number a: " + a + "\n" + "Number b: " + b + "\n" + "Number c: " + c);
+Console.WriteLine("Maximum number is: " + max);

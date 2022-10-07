e4ee344c963890403b9de7880e2b19c5a4f8b425
Valery <vakachy@gmail.com>
2022-10-07 22:57:23 +0300

    Lesson_2 Task_1 added

---
+++ Lessons/Lesson_2/Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+// 1. По двум заданным числам проверять является ли первое квадратом второго
+
+
+
+void IfPowOf2()
+{
+    Console.WriteLine("Enter natural numbers \"a\" and \"b\" to check if a = b x b");
+    
+    Console.Write("Enter a: ");
+    int a = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter b: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    if (a == Math.Pow(b, 2)) Console.Write($"Yes: {a} = {b} x {b}");
+    else Console.Write($"No: {a} != {b} x {b}");
+}
+
+IfPowOf2();

37db10477570e5d1dc6f70db5b197f0dc0434047
Valery <vakachy@gmail.com>
2022-11-17 18:35:57 +0300

    Homework: Lesson_9 Task_66 added

---
+++ Homeworks/Lesson_9/Task_66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumRecursion(int m, int n)
+{
+    if (m == n) return n;
+    else return m + SumRecursion(m + 1, n);
+}
+
+int m = 1;
+int n = 15;
+int sum = SumRecursion(m, n);
+Console.WriteLine(sum);
+
+m = 4;
+n = 8;
+sum = SumRecursion(m, n);
+Console.WriteLine(sum);
